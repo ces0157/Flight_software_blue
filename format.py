a37a7ab119f682cecbc3ec3607d5ef59581f2f80
unknown <caleb.schaefer@autonomalabs.com>
2024-04-17 15:44:55 -0500

    updated code, sd card, and exectuables

--- format.py
+++ format.py
@@ -6,10 +6,10 @@ from littlefs import LittleFS
 fs = LittleFS(block_size=512, block_count= 131072)
 
 
+#can uncomment these block's for device tree and paritions
+
 #boot = open("BOOT.BIN", mode="rb")
 #boot_data = boot.read()
-
-
 
 #with fs.open('BOOT.bin', 'wb') as fh:
     #fh.write(boot_data)
@@ -25,6 +25,11 @@ fs = LittleFS(block_size=512, block_count= 131072)
 with fs.open('first-file.txt', 'w') as fh:
     fh.write('Some text to begin with\n')
 
+with fs.open('hello_there', 'w') as fh:
+    fh.write('My data has not been changed yet\n')
+
+with fs.open('boot_count', 'wb') as fh:
+    fh.write(b'\x00')
 # Dump the filesystem content to a file
 with open('sd.img', 'wb') as fh:
     fh.write(fs.context.buffer)
